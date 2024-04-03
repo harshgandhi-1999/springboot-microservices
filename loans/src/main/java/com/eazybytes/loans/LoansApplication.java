648f2475690016f93ffc361cc6ac5cc0613a7c8a
harsh-the-innovator <harshgandhi043@gmail.com>
2024-04-03 19:32:19 +0530

    Completed loan service implementation

---
+++ loans/src/main/java/com/eazybytes/loans/LoansApplication.java
@@ -0,0 +1,29 @@
+package com.eazybytes.loans;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Loans microservice REST API Documentation",
+				description = "EazyBank Loans microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Harsh Gandhi",
+						email = "harshgandhi@gmail.com",
+						url = "https://github.com/harshgandhi-1999"
+				)
+		)
+)
+public class LoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+}
